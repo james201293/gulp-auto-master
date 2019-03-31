e36a4e13ed68fe4e1d96b1b24e6061b0b665ffe3
james <james201293@gmail.com>
2019-03-31 18:22:39 +0800

    finish 20190331

--- gulpfile.js
+++ gulpfile.js
@@ -13,20 +13,85 @@ const gulpImagemin = require('gulp-imagemin');
 //server
 const connect = require('gulp-connect');
 
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const del = require('del');
+
 //  ============================================================
 //          工作 1 建構SASS Compiler
 //  ============================================================
-
+const paths={
+	html:{
+		src:'./*.html',
+	},
+	styles:{
+		src:'./src/styles/index.scss',
+		watch:'./src/styles/**/*.scss',
+		dest:'build/css'
+	},
+	images:{
+		src:'src/images/*',
+		dest:'build/images'
+	},
+	webfonts:{
+		src:'./src/fonts/*',
+		dest:'build/font'
+	},
+}
+//const buildAssets= gulp.series(buildHtml,buildScript,buildSass,gulp.parallel(compressImage,webFont));
+//const buildVenders= gulp.series(venderJS,gulp.parallel(venderCSS,venderImage));
 
 const buildSass = function(cb){
     console.log('buildSass');
-    gulp.src('./src/styles/index.scss')
+    gulp.src('./src/styles/**/*.scss')
         .pipe(gulpSass())
-        .pipe(gulp.dest('build/'))
+        .pipe(gulp.dest('build/css'))
         .pipe(connect.reload());
     cb();
 }
 
+
+const compressAppJs = function(cb){
+    console.log('compressAppJs');
+    gulp.src('./src/app/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'))
+        .pipe(connect.reload());
+    cb();
+}
+// **抓所有子資料夾 
+const compressVenderJs = function(cb){
+    console.log('compressVenderJs');
+    gulp.src('./src/vender/**/*.js')
+        .pipe(concat('all.js'))
+        //.pipe(uglify())
+        .pipe(rename(function(path) {
+            path.basename = "venders";
+            path.basename += ".min";
+            path.extname = ".js";
+        }))
+        .pipe(gulp.dest('build/js'))
+        .pipe(connect.reload());
+    cb();
+}
+
+const compressVenderCss = function(cb){
+    console.log('compressCss');
+    gulp.src('./src/vender/**/*.css')
+        .pipe(concat('all.css'))
+        //.pipe(uglify())
+        .pipe(rename(function(path) {
+            path.basename = "venders";
+            path.basename += ".min";
+            path.extname = ".css";
+        }))
+        .pipe(gulp.dest('build/css'))
+        .pipe(connect.reload());
+    cb();
+}
+
+//不變
 const compressImage = async function(cb){
     console.log('compressImage');
     gulp.src('src/images/*')
@@ -36,6 +101,7 @@ const compressImage = async function(cb){
     cb();
 }
 
+//不變
 const webFont = async function(cb){
     console.log('webFont');
     gulp.src('./src/fonts/*')
@@ -43,6 +109,7 @@ const webFont = async function(cb){
     cb();
 }
 
+//不變
 const CSSSprite = async function(cb){
     console.log('CSSSprite');
     gulp.src('src/sprite/*.png').pipe(spritesmith({
@@ -52,6 +119,8 @@ const CSSSprite = async function(cb){
         .pipe(gulp.dest('build'));
     cb();
 }
+
+
 
 const webServer = async function(){
     console.log('reload');
@@ -92,4 +161,4 @@ gulp.watch('src/sprite/*.png', { events: 'all' }, function(cb){
 });
 
 //exports.default = buildSass;
-exports.default = gulp.series(buildSass, compressImage, webFont, CSSSprite, webServer);
+exports.default = gulp.series(buildSass, compressAppJs, compressVenderJs, compressVenderCss, compressImage, webFont, CSSSprite, webServer);
